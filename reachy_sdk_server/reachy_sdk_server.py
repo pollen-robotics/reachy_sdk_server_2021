19cece13ac104f98effd39fb973f5da965f897f8
Gaëlle Lannuzel <glannuzel@ensc.fr>
2021-02-08 16:43:37 +0100

    Change name from camera to camera_reachy

--- reachy_sdk_server/reachy_sdk_server.py
+++ reachy_sdk_server/reachy_sdk_server.py
@@ -24,7 +24,7 @@ from reachy_msgs.srv import GetJointsFullState, SetCompliant
 
 from reachy_sdk_api import joint_command_pb2 as jc_pb, joint_command_pb2_grpc
 from reachy_sdk_api import joint_state_pb2 as js_pb, joint_state_pb2_grpc
-from reachy_sdk_api import camera_pb2 as cam_pb, camera_pb2_grpc
+from reachy_sdk_api import camera_reachy_pb2 as cam_pb, camera_reachy_pb2_grpc
 from reachy_sdk_api import load_sensor_pb2 as ls_pb, load_sensor_pb2_grpc
 
 from sensor_msgs import msg
@@ -36,7 +36,7 @@ from .utils import jointstate_pb_from_request
 class ReachySDKServer(Node,
                       joint_state_pb2_grpc.JointStateServiceServicer,
                       joint_command_pb2_grpc.JointCommandServiceServicer,
-                      camera_pb2_grpc.CameraServiceServicer,
+                      camera_reachy_pb2_grpc.CameraServiceServicer,
                       load_sensor_pb2_grpc.LoadServiceServicer):
     """Reachy SDK server node."""
 
@@ -320,7 +320,7 @@ def main():
     server = grpc.server(thread_pool=ThreadPoolExecutor(max_workers=10), options=options)
     joint_state_pb2_grpc.add_JointStateServiceServicer_to_server(sdk_server, server)
     joint_command_pb2_grpc.add_JointCommandServiceServicer_to_server(sdk_server, server)
-    camera_pb2_grpc.add_CameraServiceServicer_to_server(sdk_server, server)
+    camera_reachy_pb2_grpc.add_CameraServiceServicer_to_server(sdk_server, server)
     load_sensor_pb2_grpc.add_LoadServiceServicer_to_server(sdk_server, server)
 
     server.add_insecure_port('[::]:50055')
