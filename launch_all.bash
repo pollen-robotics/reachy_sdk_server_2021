67e2380ecefcca97f2f87251125a1b5d2b970f0d
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-03-19 03:46:10 -0700

    Fix launcher.

--- launch_all.bash
+++ launch_all.bash
@@ -3,7 +3,7 @@
 # source ROS2 Foxy setup file.
 # shellcheck disable=SC1091
 source /opt/ros/foxy/setup.bash
-source /home/nuc/reachy_ws/install/setup.bash
+source $HOME/reachy_ws/install/setup.bash
 
 # Start the ROS2 launch file
-ros2 launch reachy_sdk_server run_everything_$(reachy-identify-models).launch.py
+ros2 launch reachy_sdk_server run_everything_$(reachy-identify-model).launch.py
