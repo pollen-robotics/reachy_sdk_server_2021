007108299d0ebc35ad604b5b68f30134a6343490
simheo <32677536+simheo@users.noreply.github.com>
2021-08-13 12:03:01 +0200

    Replace Popen with call.

--- reachy_sdk_server/tools.py
+++ reachy_sdk_server/tools.py
@@ -1,10 +1,9 @@
 """..."""
-from subprocess import PIPE, Popen
+from subprocess import call
 
 
 def send_service_signal(signal_type: str):
     """..."""
     if signal_type not in ['restart', 'stop']:
         raise ValueError("Signal sent to reachy_sdk_server.service should be either 'restart' or 'stop'.")
-    process = Popen(['sudo', 'systemctl', signal_type, 'reachy_sdk_server.service'], stdout=PIPE, stderr=PIPE)
-    stdout, _ = process.communicate()
+    call(['sudo', 'systemctl', signal_type, 'reachy_sdk_server.service'])
