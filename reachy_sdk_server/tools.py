688dd960e7535b3a41dbdcd02d566f29a8cc26cb
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-08-17 14:10:36 +0200

    Restart all services.

--- reachy_sdk_server/tools.py
+++ reachy_sdk_server/tools.py
@@ -1,10 +1,17 @@
 """..."""
-from subprocess import PIPE, Popen
+from subprocess import call
 
 
 def send_service_signal(signal_type: str):
     """..."""
     if signal_type not in ['restart', 'stop']:
         raise ValueError("Signal sent to reachy_sdk_server.service should be either 'restart' or 'stop'.")
-    process = Popen(['sudo', 'systemctl', signal_type, 'reachy_sdk_server.service'], stdout=PIPE, stderr=PIPE)
-    stdout, _ = process.communicate()
+
+    call(['sudo', 'systemctl', signal_type, 'reachy_sdk_server.service'])
+
+    call(['systemctl', '--user', 'stop', 'webrtc_server.service'])
+    call(['sudo', 'systemctl', signal_type, 'signaling_server.service'])
+
+    if signal_type == 'restart':
+        call(['systemctl', '--user', 'start', 'webrtc_server.service'])
+
