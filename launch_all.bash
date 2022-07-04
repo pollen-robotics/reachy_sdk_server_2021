7caf81b0adda7d17f81c0f2c2bd94f5780116286
simheo <32677536+simheo@users.noreply.github.com>
2022-07-04 11:31:08 +0200

    Remove mobile base from launch_all.bash as it is now launched separately.

--- launch_all.bash
+++ launch_all.bash
@@ -7,28 +7,10 @@ source $HOME/reachy_ws/install/setup.bash
 
 # Start the ROS2 launch file
 model=$(reachy-identify-model)
-zuuu_model=$(reachy-identify-zuuu-model)
-if [ $zuuu_model = none ];
-then
-    if [[ $model = full_kit* ]] || [[ $model = starter_kit* ]] || [[ $model = mini ]];
-    then
-        ros2 launch reachy_sdk_server run_everything.launch.py
-    else
-        ros2 launch reachy_sdk_server run_everything_no_head.launch.py
-    fi
-else 
-    if [[ $model = full_kit* ]] || [[ $model = starter_kit* ]] || [[ $model = mini ]];
-    then
-        ros2 launch reachy_sdk_server run_everything_and_zuuu.launch.py
-    else
-        ros2 launch reachy_sdk_server run_everything_no_head_and_zuuu.launch.py
-    fi
-fi
 
-# if [ $zuuu_model != none ];
-# then
-#     echo "starting mobile base launch file"
-#     ros2 launch zuuu_description zuuu_bringup_low_level_only.launch.py
-# else
-#     echo "Model with no mobile base"
-# fi
+if [[ $model = full_kit* ]] || [[ $model = starter_kit* ]] || [[ $model = mini ]];
+then
+    ros2 launch reachy_sdk_server run_everything.launch.py
+else
+    ros2 launch reachy_sdk_server run_everything_no_head.launch.py
+fi
